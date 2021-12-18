824e540b3566949d78241fb8a6d1f069985a5ac3
daramayo90 <da.aramayo1990@gmail.com>
2021-12-18 14:39:44 -0500

    Component: Presentation. Initial HTML

--- src/components/presentation/Presentation.js
+++ src/components/presentation/Presentation.js
@@ -9,27 +9,37 @@ function Presentation () {
             </div>
 
             <div>
-                <h1>
-                    Are You Looking For A <br />
-                    Freelance Jamstack Developer?
-                </h1>
+                <h1>Hi there, I'm Damián 👋</h1>
+                <p>
+                    I’m a <strong>Full Stack Web Developer</strong> based in Buenos Aires, Argentina.
+                    <br />
+                    I started building websites using technologies such as Wordpress, HTML, CSS and JavaScript.
+                    <br /><br />
+                    I take my craft seriously, having invested in taking my skills, improving myself every day to learn something new to push me forward in this learning path.
+                    <br/><br />
+                    I specialise in <strong>JavaScript</strong> and <strong>React.JS</strong> development. 
+                    If you’re looking for an enthusiastic and proactive Web Developer, I’m confident that I can use my skills and expertise to help you!
+                </p>
             </div>
 
             <div>
-                <p>Do you need someone who is:</p>
+                <p>Do you need someone who:</p>
 
                 <ul>
-                    <li>Skilled at building fast, beautiful websites with the latest technology?</li>
-                    <li>Comfortable both working in a team and individually?</li>
-                    <li>Confident with the whole jamstack ecosystem?</li>
+                    <li>Is super comfortable working in a team of developers, designers, or in a project team?</li>
+                    <li>Is skilled at building fast, beautiful websites with the latest technology?</li>
+                    <li>Is confident with problems solving?</li>
+                    <li>Is proactive and quick learner?</li>
+                    <li>Has demonstrated experience working with Agile Methodologies using Jira and Confluence?</li>
+                    <li>Has ability to lead complex or multiple projects?</li>
+                    <li>Has a Business and IT understanding?</li>
                 </ul>
 
                 <p><b>You're in the right place</b>, here's what to do next:</p>
             </div>
 
             <div>
-                <button>Hire me</button>
-                <button>Find out more</button>
+                <button>Contact Me</button>
             </div>
         </section>
     );
