eab8c707ce0ae87d1e1399ed800fc13f9da69097
daramayo90 <da.aramayo1990@gmail.com>
2022-03-27 19:37:25 -0500

    weather app added

--- src/components/my-projects/ProjectsList.js
+++ src/components/my-projects/ProjectsList.js
@@ -4,8 +4,18 @@ import netflix from './images/netflix-desktop.jpg';
 import netflixMobile from './images/netflix-mobile.jpg';
 import viandas from './images/viandas-desktop.jpg';
 import viandasMobile from './images/viandas-mobile.jpg';
+import weather from './images/wather-desktop.jpg';
+import weatherMobile from './images/wather-mobile.jpg';
 
 export const list = [
+    {
+        name: 'Netflix - React & TypeScript',
+        imageDesktop: netflix,
+        imageMobile: netflixMobile,
+        description: "The backend has an MCV structure and a connection to a MongoDB cloud database which stores registered users. User login/registration was developed using JWT and hashing passwords.\n\nThe Frontend of this project is built in React JS and TypeScript was used in some components. Axios was used for the connection with the backend.\n\nEach registered user is stored in local storage (without the password), so data is not lost when browsers are refreshed or closed.\n\nRegarding the movies, the information was taken from an external API (TMDB).",
+        github: "https://github.com/daramayo90/react-netflix-ts/",
+        link: "https://react-netflix-ts.herokuapp.com/"
+    },
     {
         name: 'Calculator - React',
         imageDesktop: calculator,
@@ -15,12 +25,12 @@ export const list = [
         link: "https://daramayo90.github.io/react-calculator/"
     },
     {
-        name: 'Netflix - React & TypeScript',
-        imageDesktop: netflix,
-        imageMobile: netflixMobile,
-        description: "The backend has an MCV structure and a connection to a MongoDB cloud database which stores registered users. User login/registration was developed using JWT and hashing passwords.\n\nThe Frontend of this project is built in React JS and TypeScript was used in some components. Axios was used for the connection with the backend.\n\nEach registered user is stored in local storage (without the password), so data is not lost when browsers are refreshed or closed.\n\nRegarding the movies, the information was taken from an external API (TMDB).",
-        github: "https://github.com/daramayo90/react-netflix-ts/",
-        link: "https://react-netflix-ts.herokuapp.com/"
+        name: 'Weather Web App',
+        imageDesktop: weather,
+        imageMobile: weatherMobile,
+        description: "Real-time or current JSON weather and XML weather API provides access to near real-time weather information for millions of locations worldwide.",
+        github: "https://github.com/daramayo90/weather-webapp",
+        link: "https://free-weather-web-api.herokuapp.com/"
     },
     {
         name: 'Viandas Cook - WordPress',
